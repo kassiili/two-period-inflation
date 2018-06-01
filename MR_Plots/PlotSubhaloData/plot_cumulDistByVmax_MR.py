c04b7fb68cd7869c0b401f2b244341ee3f78d0f4
Kasper Siilin <kasper.siilin@helsinki.fi>
2018-06-01 11:37:14 +0300

    Added particle slice plot with subhalo centre of masses and modified plot_cumulDistByVmax_MR.py to divide between isolated galaxies and satellites.

--- MR_Plots/PlotSubhaloData/plot_cumulDistByVmax_MR.py
+++ MR_Plots/PlotSubhaloData/plot_cumulDistByVmax_MR.py
@@ -2,6 +2,7 @@ import numpy as np
 import h5py
 import time
 import matplotlib.pyplot as plt
+import matplotlib
 from read_subhaloData_MR import read_subhaloData
 from read_header_MR import read_header
 
@@ -10,13 +11,17 @@ class plot_subhalo_dist_vs_vmax:
     def __init__(self):
         maxVelocities = read_subhaloData('Vmax')
         stellarMasses = read_subhaloData('Stars/Mass')
-        self.maxVelocitiesLum = maxVelocities[np.logical_and(maxVelocities > 0, stellarMasses > 0)]
-        self.maxVelocitiesDark = maxVelocities[np.logical_and(maxVelocities > 0, stellarMasses == 0)]
+        subGroupNumbers = read_subhaloData('SubGroupNumber')
+        self.maxVelocitiesSatLum = maxVelocities[np.logical_and.reduce((maxVelocities > 0, stellarMasses > 0, subGroupNumbers != 0))]
+        self.maxVelocitiesSatDark = maxVelocities[np.logical_and.reduce((maxVelocities > 0, stellarMasses == 0, subGroupNumbers != 0))]
+        self.maxVelocitiesIsolLum = maxVelocities[np.logical_and.reduce((maxVelocities > 0, stellarMasses > 0, subGroupNumbers == 0))]
+        self.maxVelocitiesIsolDark = maxVelocities[np.logical_and.reduce((maxVelocities > 0, stellarMasses == 0, subGroupNumbers == 0))]
 
-        print(self.maxVelocitiesDark.size)
         #Sort arrays in descending order:
-        self.maxVelocitiesLum[::-1].sort()
-        self.maxVelocitiesDark[::-1].sort()
+        self.maxVelocitiesSatLum[::-1].sort()
+        self.maxVelocitiesSatDark[::-1].sort()
+        self.maxVelocitiesIsolLum[::-1].sort()
+        self.maxVelocitiesIsolDark[::-1].sort()
 
 
     def plot(self):
@@ -25,13 +30,17 @@ class plot_subhalo_dist_vs_vmax:
 
         axes.set_xscale('log')
         axes.set_yscale('log')
-        axes.plot(self.maxVelocitiesLum, np.arange(1, self.maxVelocitiesLum.size + 1), c='black')
-        axes.plot(self.maxVelocitiesDark, np.arange(1, self.maxVelocitiesDark.size + 1), c='grey')
+        axes.plot(self.maxVelocitiesSatLum, np.arange(1, self.maxVelocitiesSatLum.size + 1), c='lightblue', label='Luminous sat. galaxies')
+        axes.plot(self.maxVelocitiesSatDark, np.arange(1, self.maxVelocitiesSatDark.size + 1), c='blue', label='Dark sat. galaxies')
+        axes.plot(self.maxVelocitiesIsolLum, np.arange(1, self.maxVelocitiesIsolLum.size + 1), c='pink', label='Luminous isol. galaxies')
+        axes.plot(self.maxVelocitiesIsolDark, np.arange(1, self.maxVelocitiesIsolDark.size + 1), c='red', label='Dark isol. galaxies')
 
+        axes.legend()
         axes.set_xlabel('$v_{max} [\mathrm{km s^{-1}}]$')
         axes.set_ylabel('$N(>v_{max})$')
         axes.set_title('Distribution of luminous subhaloes as a function of $v_{max}$')
 
+        plt.show()
         plt.savefig('dist-of-subhaloes-as-function-of-Vmax_MR.png')
         plt.close()
 
