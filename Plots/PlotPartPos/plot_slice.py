ac6e72650950412e98f66c86c995ff999f8af560
Kasper Siilin <kasper.siilin@helsinki.fi>
2018-07-02 13:33:51 +0300

    Added COP of central galaxies to the plot_slice plots

--- Plots/PlotPartPos/plot_slice.py
+++ Plots/PlotPartPos/plot_slice.py
@@ -8,13 +8,17 @@ from read_dataset import read_dataset
 sys.path.insert(0, '/home/kassiili/SummerProject/practise-with-datasets/Plots/')
 from read_header import read_header
 
+sys.path.insert(0, '/home/kassiili/SummerProject/practise-with-datasets/Plots/PlotSubhaloData')
+from read_subhaloData import read_subhaloData
+
 class plot_slice:
 
-    def __init__(self, part_type, dataset='LR'):
+    def __init__(self, part_type, slice_axis, dataset='LR'):
         self.part_type = part_type
+        self.slice_axis = slice_axis
         self.dataset = dataset
         self.a, self.h, mass, self.boxsize = read_header(dataset=self.dataset) 
-        self.width = 0.2
+        self.width = 0.03
         self.coords = read_dataset(self.part_type, 'Coordinates', dataset=self.dataset) * u.cm.to(u.Mpc)
 
         self.cm = self.calcCM()
@@ -32,28 +36,39 @@ class plot_slice:
         return np.mean(dmCoords, axis=0)
 
     def compute_slice(self):
-        min0 = self.cm[0]-self.width/2
-        max0 = self.cm[0]+self.width/2
-        self.coords = self.coords[np.logical_and(min0 < self.coords[:,0], self.coords[:,0] < max0),:] 
+        min0 = self.cm[self.slice_axis]-self.width/2
+        max0 = self.cm[self.slice_axis]+self.width/2
+        self.coords = self.coords[np.logical_and(min0 < self.coords[:,self.slice_axis], self.coords[:,self.slice_axis] < max0),:] 
 
     def plot(self):
         plt.figure()
         axes = plt.gca() 
-        axes.set_xlim([self.cm[1]-5, self.cm[1]+5])
-        axes.set_ylim([self.cm[2]-5, self.cm[2]+5])
 
-        axes.scatter(self.coords[:,1], self.coords[:,2], s=0.01)
+        help = [0,1,2]
+        x=help[self.slice_axis-2]; y=help[self.slice_axis-1]
+        axes.set_xlim([self.cm[x]-7, self.cm[x]+7])
+        axes.set_ylim([self.cm[y]-7, self.cm[y]+7])
 
-        axes.set_title('Particles (type %i) in a volume slice centered on a LG analogue'%self.part_type)
+        axes.scatter(self.coords[:,x], self.coords[:,y], s=0.01)
 
-        plt.show()
-#        plt.savefig('slice_partType%i%s.png'%(self.part_type, self.dataset)) 
-#        plt.close()
+        gns = read_subhaloData("GroupNumber", dataset=self.dataset)
+        sgns = read_subhaloData("SubGroupNumber", dataset=self.dataset)
+        COPs = read_subhaloData("CentreOfPotential", dataset=self.dataset)
 
-#slice = plot_slice(4, dataset='LR')
-#slice.plot()
-part_types = [0,1,4]
-for n in part_types:
-    slice = plot_slice(n, dataset='MR') 
-    slice.plot()
+        mask = np.logical_or(np.logical_and(gns == 1, sgns == 0), np.logical_and(gns == 2, sgns == 0))
+        centrals = COPs[mask] * u.cm.to(u.Mpc)
+        axes.scatter(centrals[:,x], centrals[:,y], s=10, c='red', edgecolor='none')
+
+#        axes.set_title('Particles (type %i) in a volume slice centered on a LG analogue'%self.part_type)
+
+#        plt.show()
+        plt.savefig('slice_partType%i%s.png'%(self.part_type, self.dataset)) 
+        plt.close()
+
+slice = plot_slice(1, 0, dataset='MR')
+slice.plot()
+#part_types = [0,1,4]
+#for n in part_types:
+#    slice = plot_slice(n, dataset='MR') 
+#    slice.plot()
 
