2afe2d3e4a7c51f66006d715d8f67b1c88762023
Kasper Siilin <kasper.siilin@helsinki.fi>
2018-07-26 17:24:53 +0300

    Modified plot_SM_vs_Vmax script to allow plotting from multiple datasets
    The code was divided into two classes, first of which is responsible for
    transforming data from a given dataset into plottable form, and the
    second for handling the actual plot.

--- PlotScripts/PlotSubhaloData/plot_SM_vs_Vmax.py
+++ PlotScripts/PlotSubhaloData/plot_SM_vs_Vmax.py
@@ -9,7 +9,7 @@ from calc_median import calc_median_trend
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../ReadData"))
 import read_data as read_data
 
-class plot_SM_vs_Vmax:
+class SM_vs_Vmax_data:
 
     def __init__(self, dataset='V1_LR_fix_127_z000p000', nfiles_part=16, nfiles_group=96):
 
@@ -18,45 +18,78 @@ class plot_SM_vs_Vmax:
         self.nfiles_group = nfiles_group
         self.reader = read_data.read_data(dataset=self.dataset, nfiles_part=self.nfiles_part, nfiles_group=self.nfiles_group)
 
-        maxVelocities = self.reader.read_subhaloData('Vmax') / 100000   # cm/s to km/s
-        stellarMasses = self.reader.read_subhaloData('Stars/Mass') * u.g.to(u.Msun)
-        self.subGroupNumbers = self.reader.read_subhaloData('SubGroupNumber')
-        maskSat = np.logical_and.reduce((maxVelocities > 0, stellarMasses > 0, self.subGroupNumbers != 0))
-        maskIsol = np.logical_and.reduce((maxVelocities > 0, stellarMasses > 0, self.subGroupNumbers == 0))
+        self.read_galaxies()
 
-        self.maxVelocitiesSat = maxVelocities[maskSat]
-        self.stellarMassesSat = stellarMasses[maskSat]
-        self.maxVelocitiesIsol = maxVelocities[maskIsol]
-        self.stellarMassesIsol = stellarMasses[maskIsol]
+    def read_galaxies(self):
 
-    def plot(self):
-        fig, axes = plt.subplots()
+        # Read subhalo data:
+        vmax = self.reader.read_subhaloData('Vmax') / 100000   # cm/s to km/s
+        SM = self.reader.read_subhaloData('Stars/Mass') * u.g.to(u.Msun)
+        SGNs = self.reader.read_subhaloData('SubGroupNumber')
 
-        axes.set_xscale('log')
-        axes.set_yscale('log')
+        # Divide into satellites and isolated galaxies:
+        maskSat = np.logical_and.reduce((vmax > 0, SM > 0, SGNs != 0))
+        maskIsol = np.logical_and.reduce((vmax > 0, SM > 0, SGNs == 0))
 
-        axes.scatter(self.maxVelocitiesSat, self.stellarMassesSat, s=3, c='red', edgecolor='none', label='satellite galaxies')
-        axes.scatter(self.maxVelocitiesIsol, self.stellarMassesIsol, s=3, c='blue', edgecolor='none', label='isolated galaxies')
+        self.vmaxSat = vmax[maskSat]
+        self.SMSat = SM[maskSat]
+        self.vmaxIsol = vmax[maskIsol]
+        self.SMIsol = SM[maskIsol]
 
-        median = calc_median_trend(self.maxVelocitiesSat, self.stellarMassesSat, points_per_bar=7)
-        axes.plot(median[0], median[1], c='red', linestyle='--')
-        #axes.scatter(median[0], median[1], s=5)
 
-        median = calc_median_trend(self.maxVelocitiesIsol, self.stellarMassesIsol)
-        axes.plot(median[0], median[1], c='blue', linestyle='--')
+class plot_SM_vs_Vmax:
 
-        axes.set_xlim(10, 100)
-        axes.set_ylim(10**6, 10**10)
-        axes.legend(loc=0)
-        axes.set_xlabel('$v_{max}[\mathrm{km s^{-1}}]$')
-        axes.set_ylabel('$M_*[\mathrm{M_\odot}]$')
-        axes.set_title('Stellar mass of luminous subhaloes')
+    def __init__(self):
+        """ Create new figure with stellar mass on y-axis and Vmax on x-axis. """
+    
+        self.fig, self.axes = plt.subplots()
+        self.set_axes()
+        self.set_labels()
+        
+    def set_axes(self):
+        """ Set shapes for axes. """
 
-        #plt.show()
-        fig.savefig('../Figures/%s/SM_vs_Vmax.png'%self.dataset)
+        self.axes.set_xscale('log')
+        self.axes.set_yscale('log')
+        self.axes.set_xlim(10, 100)
+        self.axes.set_ylim(10**6, 10**10)
+        
+    def set_labels(self):
+        """ Set labels. """
+
+        self.axes.set_xlabel('$v_{max}[\mathrm{km s^{-1}}]$')
+        self.axes.set_ylabel('$M_*[\mathrm{M_\odot}]$')
+        self.axes.set_title('Stellar mass of luminous subhaloes')
+
+    def add_data(self, data, satellites, col):
+        """ Plot data (object of type SM_vs_Vmax_data) into an existing figure. Satellites is a boolean variable with value 1, if satellites are to be plotted, and 0, if instead isolated galaxies are to be plotted. """
+
+        x = 0; y = 0
+        if satellites:
+            x = data.vmaxSat; y = data.SMSat
+        else:
+            x = data.vmaxIsol; y = data.SMIsol
+
+        self.axes.scatter(x, y, s=3, c=col, edgecolor='none', label=data.dataset)
+        median = calc_median_trend(x, y, points_per_bar=7)
+        self.axes.plot(median[0], median[1], c=col, linestyle='--')
+        #self.axes.scatter(median[0], median[1], s=5)
+    
+    def save_figure(self):
+        """ Save figure. """
+        
+        self.axes.legend(loc=0)
+        plt.show()
+        self.fig.savefig('../Figures/Comparisons_082_z001p941/SM_vs_Vmax.png')
         plt.close()
 
 
-plot = plot_SM_vs_Vmax(dataset='V1_MR_fix_082_z001p941', nfiles_part=16, nfiles_group=192)
-plot.plot()
+plot = plot_SM_vs_Vmax()
+LCDM = SM_vs_Vmax_data(dataset='V1_MR_fix_082_z001p941', nfiles_part=16, nfiles_group=192)
+curvaton = SM_vs_Vmax_data(dataset='V1_MR_mock_1_fix_082_z001p941', nfiles_part=1, nfiles_group=64)
+
+plot.add_data(LCDM, 1, 'red')
+plot.add_data(curvaton, 1, 'blue')
+plot.save_figure() 
+    
 
