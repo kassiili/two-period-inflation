42eeeedba6af97daec9c16e06f5b03f1363a6784
Kasper Siilin <kasper.siilin@helsinki.fi>
2020-02-24 20:44:03 +0200

    Dataset class now replaces the read_data functions for reading subhalos
    The Dataset class now combines all the hdf5-files into a single File
    object, which is constructed in the new function make_group_file, which
    is called in __init__. The read_subhalo_attr function is basically
    copied from the old module: it reads the given attribute values from the
    data files. get_subhalos is the public interface, which uses
    read_subhalo_attr. It allows for splitting the dataset into satellites
    and isolated galaxies. The retrieval of the datasets might have been
    slowed down, since other conditions (like SM > 0) are not imposed on the
    datasets in these functions.

--- Test/test_Dataset.py
+++ Test/test_Dataset.py
@@ -1,7 +1,8 @@
 import sys,os 
+import h5py
 
 sys.path.append(os.path.join(os.path.dirname(__file__),".."))
-from PlotSubhaloData.dataset import Dataset
+from dataset import Dataset
 
 def test_get_data_path(dataset):
     print(dataset.get_data_path("part"))
@@ -12,6 +13,30 @@ def test_get_data_path(dataset):
 def test_count_files(dataset):
     print(dataset.count_files())
 
+def test_make_group_file(dataset):
+    filename = dataset.grp_file
+    print(filename)
+    with h5py.File(filename,'r') as grpf:
+        print("# of links:",len(grpf.keys()))
+        print("link1 contents:",grpf['link1'].keys())
+        print("link1/Subhalo contents:",grpf['link1/Subhalo'].keys())
+        print("link78/Subhalo/Vmax contents:",grpf['link78/Subhalo/Vmax'])
+
+def test_read_subhalo_attr(dataset):
+    attr = 'Vmax'
+    print(len(dataset.read_subhalo_attr(attr)))
+
+def test_get_subhalos(dataset):
+    attr = "Vmax"
+    print(len(dataset.get_subhalos(attr, False)[0]))
+    sat, isol = dataset.get_subhalos(attr, True)
+    print(len(sat))
+    print(len(isol))
+
+
 LCDM = Dataset("V1_LR_fix_127_z000p000","LCDM")
-test_get_data_path(LCDM)
-test_count_files(LCDM)
+#test_get_data_path(LCDM)
+#test_count_files(LCDM)
+#test_make_group_file(LCDM)
+test_read_subhalo_attr(LCDM)
+test_get_subhalos(LCDM)
---
+++ dataset.py
@@ -0,0 +1,207 @@
+import os
+import re
+from pathlib import Path
+import glob
+import numpy as np
+import h5py
+
+class Dataset:
+
+    def __init__(self, ID, name):
+        """
+        Parameters
+        ----------
+        ID : str
+            identifier of the simulation data -- should be equivalent to
+            the directory name of the snapshot
+        name : str
+            label of the data set
+        """
+
+        self.ID = ID
+        self.name = name
+        self.grp_file = 'groups_{}.hdf5'.format(name)
+        self.make_group_file()
+        
+    def count_files(self):
+        """ Count the relevant data files in the head directory of the
+        dataset. 
+
+        Returns
+        -------
+        cnt : int
+            number of data files
+        """
+
+        groupfs = os.listdir(self.get_data_path("group"))
+        partfs = os.listdir(self.get_data_path("part"))
+
+        cnt = {}
+        # Exclude irrelevant files and count:
+        cnt["group"] = \
+        sum([bool(re.match("eagle_subfind_tab_127_z000p000.*",f)) \
+            for f in groupfs])
+
+        cnt["part"] = sum([bool(re.match("snap_127_z000p000.*",f)) \
+            for f in partfs])
+
+        return cnt
+
+    def get_data_path(self, datatype):
+        """ Constructs the path to data directory. 
+        
+        Paramaters
+        ----------
+        datatype : str
+            recognized values are: part and group
+
+        Returns
+        -------
+        path : str
+            path to data directory
+        """
+
+        home = os.path.dirname(os.getcwd())
+        path = os.path.join(home,"snapshots",self.ID)
+        direc = ""
+        if datatype == "part":
+            direc = "snapshot_"
+        elif datatype == "group":
+            direc += "groups_"
+        else:
+            return None
+
+        fields = self.ID.split("_")
+        direc += fields[-2] + "_" + fields[-1]
+
+        return os.path.join(path,direc)
+
+    def get_file_prefix(self, datatype):
+        """ Constructs file prefix for individual data files. 
+        
+        Paramaters
+        ----------
+        datatype : str
+            recognized values are: part and group
+
+        Returns
+        -------
+        prefix : str
+            file prefix
+        """
+
+        prefix = ""
+        if datatype == "part":
+            prefix = "snap"
+        elif datatype == "group":
+            prefix += "eagle_subfind_tab_"
+        else:
+            return None
+
+        fields = self.ID.split("_")
+        prefix += fields[-2] + "_" + fields[-1]
+
+        return prefix
+
+    def get_subhalos(self, attr, divided):
+        """ Retrieves the given attribute values for subhaloes in the
+        dataset.
+        
+        Parameters
+        ----------
+        attr : str
+            attribute to be retrieved
+        divided : bool
+            if True, output is divided into satellites and isolated
+            galaxies
+
+        Returns
+        -------
+        data : tuple
+            tuple of one or two entries, depending on the value of the
+            argument "divided". If divided == True, return satellite data
+            in the first entry and isolated galaxies data in the second.
+        """
+
+        data = self.read_subhalo_attr(attr)
+
+        if divided:
+            SGNs = self.read_subhalo_attr('SubGroupNumber')
+            print(len(SGNs))
+
+            # Divide into satellites and isolated galaxies:
+            dataSat = data[SGNs != 0]
+            dataIsol = data[SGNs == 0]
+
+            return (dataSat,dataIsol)
+        else:
+            return (data,)
+
+    def read_subhalo_attr(self, attr):
+        """ Reads the data files for the attribute "attr" of each subhalo.
+        
+        Parameters
+        ----------
+        attr : str
+            attribute to be retrieved
+
+        Returns
+        -------
+        out : HDF5 dataset
+            dataset of the values of attribute "attr" for each subhalo
+        """
+
+        # Output array.
+        out = []
+
+        # Get group file:
+        with h5py.File(self.grp_file,'r') as grpf:
+
+            # Loop over each file and extract the data.
+            for f in grpf.values():
+                tmp = f['Subhalo/{}'.format(attr)][...]
+                out.append(tmp)
+        
+                # Get conversion factors.
+                cgs     = f['Subhalo/{}'.format(attr)].attrs\
+                        .get('CGSConversionFactor')
+                aexp    = f['Subhalo/{}'.format(attr)].attrs\
+                        .get('aexp-scale-exponent')
+                hexp    = f['Subhalo/{}'.format(attr)].attrs\
+                        .get('h-scale-exponent')
+        
+                # Get expansion factor and Hubble parameter from the 
+                # header.
+                a       = f['Header'].attrs.get('Time')
+                h       = f['Header'].attrs.get('HubbleParam')
+        
+#                f.close() # Do I need to close external links???
+        
+            # Combine to a single array.
+            if len(tmp.shape) > 1:
+                out = np.vstack(out)
+            else:
+                out = np.concatenate(out)
+        
+            # Convert to physical and return in cgs units.
+            if out.dtype != np.int32 and out.dtype != np.int64:
+                out = np.multiply(out, cgs * a**aexp * h**hexp, dtype='f8')
+    
+        return out
+
+    def make_group_file(self):
+        """ Create a group file and add links to all the data files """
+
+        # Create the file object with links to all the files:
+        grpf = h5py.File(self.grp_file,'w')
+        path = self.get_data_path('group')
+
+        # Iterate through group files (and dismiss irrelevant files in the
+        # directory):
+        for i,filename in \
+        enumerate(glob.glob(os.path.join(path,'eagle_subfind_tab*'))):
+            # Make an external link:
+            grpf['link{}'.format(i)] = h5py.ExternalLink(filename,'/')
+
+        grpf.close()
+
